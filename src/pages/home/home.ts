53ac7711faaf6418e1008ab804a89b2e7be174ed
maf131159 <manoelantoniofernandes@gmail.com>
2022-03-03 18:50:49 -0300

    Desabilitar Menu na tela inicial

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuController } from 'ionic-angular';
 import { IonicPage } from 'ionic-angular';
 import { NavController } from 'ionic-angular';
 
@@ -9,9 +10,16 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public menu:MenuController) {
   }
+
+  ionViewWillEnter() {
+    this.menu.swipeEnable(false); 
+  }
+  ionViewDidLeave() {
+    this.menu.swipeEnable(true);
+  }
+
   login() {
     this.navCtrl.setRoot("CategoriasPage");
   }
