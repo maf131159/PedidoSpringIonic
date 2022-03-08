6c3c8032b208a5b6cf09774bba3c812d606c9c2b
maf131159 <manoelantoniofernandes@gmail.com>
2022-03-08 19:43:50 -0300

    Mostrando as categorias

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { API_CONFIG } from '../../config/api.config';
 import { CategoriaService } from '../../services/domain/categoria.service';
 
 /**
@@ -16,13 +17,19 @@ import { CategoriaService } from '../../services/domain/categoria.service';
 })
 export class CategoriasPage {
 
+  bucketUrl: string = API_CONFIG.bucketBaseUrl;
+
+  items: CategoriaDTO[];
+
   constructor(
       public navCtrl: NavController, public navParams: NavParams,
       public categoriaService: CategoriaService) {
   }
 
   ionViewDidLoad() {
-    this.categoriaService.findAll().subscribe(response => {console.log(response);
+    this.categoriaService.findAll()
+      .subscribe(response => {
+        this.items = response;
     },
     error => {
       console.log(error);
